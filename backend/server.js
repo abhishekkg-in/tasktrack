4e0181d5a9d9ab6008f549399151c46917e628a6
Abhishek Gupta <abhishek.61095728@ltimindtree.com>
2024-11-04 16:18:02 +0530

    successfully connected mongodb database with backend

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,23 @@
-console.log("world")
+const express = require('express')
+const cors = require('cors')
+const colors = require('colors')
+const dotenv = require('dotenv').config()
+const connectDB = require('./config/db')
+// const { errorHandler } = require('./middleware/errorMiddleware')
+const port = process.env.PORT || 3001
+
+// initializing app
+const app = express()
+
+// Middleware
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false })) // url encoded
+
+// connecting to database
+connectDB()
+
+
+app.listen(port, () => {
+    console.log(`Server running at ${port}...`)
+})
