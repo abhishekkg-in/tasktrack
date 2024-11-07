fe5fd323a149ddf1613268fa1b477f47675a2613
Abhishek Gupta <abhishek.61095728@ltimindtree.com>
2024-11-07 07:41:31 +0530

    updated session time for jwt

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -110,7 +110,6 @@ const deleteUsers = asyncHandler(async (req, res) => {
 
 // Generate JWT
 const generateToken = (id) => {
-    
     return jwt.sign({id}, process.env.JWT_SECRET, {expiresIn: '1h'})
 }
 
